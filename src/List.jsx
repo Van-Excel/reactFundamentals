029d6b78eff893e538f554d5f9e67e52ee26d8a6
Van-Excel <vanexcel.investments@gmail.com>
2024-02-09 22:02:00 +0000

    list

--- src/List.jsx
+++ src/List.jsx
@@ -1,13 +1,28 @@
 
-
-
 // rendering a list
-export default function List() {
-const fruits = ['apple', 'orange', 'banana', 'ginger']
+function SimpleList() {
 
-const listItems = fruits.map((fruit) => {<li>{fruit}</li>})
+
+    const fruits = ['apple', 'orange', 'banana', 'ginger']
+    const listItems = fruits.map((fruit) => {<li>{fruit}</li>})
+
+
+    return(
+    <>
+     <div><ul>{listItems}</ul></div>
+    
+    </>
+    )
+}
+
+export default function List() {
+
 
   return (
-    <div><ul>{listItems}</ul></div>
+    <>
+    
+    <SimpleList />
+    </>
+   
   )
 }
